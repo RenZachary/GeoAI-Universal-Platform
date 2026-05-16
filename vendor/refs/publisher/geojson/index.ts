ff2575f05d4df71c810e5d4fc3bfe79cffaba917
rzcgis <rzcgis@foxmail.com>
2026-05-16 21:13:18 +0800

    refactor: remove deprecated vendor publisher files
    - Delete old publisher implementations from vendor/refs/publisher/
    - These have been replaced by new modular publisher architecture in server/src/publishers/
    - Clean up deprecated code as part of service layer refactoring

--- vendor/refs/publisher/geojson/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * GeoJSON MVT Publisher Module
- * 
- * Exports MVT publisher service for converting GeoJSON to vector tiles.
- */
-
-export { GeoJSONMVTPublisher } from './GeoJSONMVTPublisher';
-export { type MVTTileCallback, type MVTTileRequest, type MVTPublisherOptions } from '../MVTPublisherTypes';
